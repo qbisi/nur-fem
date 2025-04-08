eac4b1e9a96a1606b0ba457d4e2c7f0d61c32634
qbisi <qbisicwate@gmail.com>
2025-04-08 17:00:31 +0800

    fix override

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -1,12 +1,12 @@
 {
-  systems = [
-    "x86_64-linux"
-    "aarch64-linux"
-    "x86_64-darwin"
-    "aarch64-darwin"
-  ];
+  # systems = [
+  #   "x86_64-linux"
+  #   "aarch64-linux"
+  #   "x86_64-darwin"
+  #   "aarch64-darwin"
+  # ];
 
-  imports = [ ./python-packages.nix ];
+  # imports = [ ./python-packages.nix ];
 
   perSystem =
     {
@@ -19,13 +19,82 @@
     {
       legacyPackages = lib.makeScope pkgs.newScope (
         self:
+        let
+          pythonOverrides =
+            final: prev:
+            lib.packagesFromDirectoryRecursive {
+              inherit (final) callPackage;
+              directory = ./python-by-name;
+            }
+            // {
+              pkgs = prev.pkgs // self;
+
+              petsc4py = prev.toPythonModule (
+                final.pkgs.petsc.override {
+                  python3 = prev.python;
+                  python3Packages = final;
+                  pythonSupport = true;
+                }
+              );
+
+              slepc4py = prev.toPythonModule (
+                final.pkgs.slepc.override {
+                  pythonSupport = true;
+                  python3 = prev.python;
+                  python3Packages = final;
+                  petsc = final.petsc4py;
+                }
+              );
+
+              netgen-mesher = prev.toPythonModule (
+                final.pkgs.netgen.override {
+                  python3Packages = final;
+                }
+              );
+
+              ngsolve = prev.toPythonModule (
+                final.pkgs.ngsolve.override {
+                  python3Packages = final;
+                }
+              );
+
+              adios2 = prev.toPythonModule (
+                final.pkgs.adios2.override {
+                  pythonSupport = true;
+                  python3 = prev.python;
+                  python3Packages = final;
+                }
+              );
+
+              kahip = prev.toPythonModule (
+                final.pkgs.kahip.override {
+                  pythonSupport = true;
+                  python3 = prev.python;
+                  python3Packages = final;
+                }
+              );
+            };
+        in
         lib.packagesFromDirectoryRecursive {
           inherit (self) callPackage;
           directory = ./by-name;
         }
         // {
-          petscPackages = self.petsc.petscPackages;
-          python3Packages = config.legacyPackages.python312Packages;
+          python312 = pkgs.python312.override (old: {
+            packageOverrides = lib.composeExtensions (old.packageOverrides or (_: _: { })) pythonOverrides;
+          });
+
+          python313 = pkgs.python312.override (old: {
+            packageOverrides = lib.composeExtensions (old.packageOverrides or (_: _: { })) pythonOverrides;
+          });
+
+          python3 = self.python312;
+
+          python312Packages = lib.recurseIntoAttrs self.python312.pkgs;
+
+          python313Packages = lib.recurseIntoAttrs self.python313.pkgs;
+
+          python3Packages = self.python312Packages;
         }
       );
     };
