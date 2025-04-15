07ac7a9a4dc71bd8e4f0a3d008e6d1ba3005765b
qbisi <qbisicwate@gmail.com>
2025-04-16 03:40:56 +0800

    add RAJA

---
+++ pkgs/by-name/raja/package.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  llvmPackages,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  version = "2025.03.0";
+  pname = "raja";
+
+  src = fetchFromGitHub {
+    owner = "LLNL";
+    repo = "RAJA";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-OLQC3h/BKprGkpR978LkyLpIqNj5fmyBLlry3bOczCk=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = [
+  ] ++ lib.optional stdenv.cc.isClang llvmPackages.openmp;
+
+  cmakeFlags = [
+    (lib.cmakeBool "ENABLE_OPENMP" true)
+    (lib.cmakeBool "RAJA_ENABLE_EXAMPLES" false)
+    (lib.cmakeBool "RAJA_ENABLE_EXERCISES" false)
+    # (lib.cmakeBool "RAJA_ENABLE_DOCUMENTATION" true)
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!stdenv.hostPlatform.isStatic))
+    (lib.cmakeBool "RAJA_ENABLE_TESTS" finalAttrs.finalPackage.doCheck)
+  ];
+
+  doCheck = true;
+
+  meta = {
+    homepage = "https://github.com/LLNL/RAJA";
+    description = "RAJA Performance Portability Layer (C++)";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ qbisi ];
+  };
+})
