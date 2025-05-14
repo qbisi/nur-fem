b885182b194b68d3b3d0ebe64e021c63a682945f
qbisi <qbisicwate@gmail.com>
2025-05-15 01:47:37 +0800

    pytest-pyvista: init at 0.1.9

---
+++ pkgs/python-by-name/pytest-pyvista/package.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  pkgs,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flit-core,
+  pytest,
+  pyvista,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-pyvista";
+  version = "0.1.9";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pyvista";
+    repo = "pytest-pyvista";
+    tag = "v${version}";
+    hash = "sha256-E+1PV0tBKzwqERytsTc8YA2IiRezmUSK3UXSEzLSpxg=";
+  };
+
+  build-system = [ flit-core ];
+
+  dependencies = [
+    pytest
+  ];
+
+  pythonImportsCheck = [ "pytest_pyvista" ];
+
+  nativeCheckInputs = [
+    pyvista
+    pytestCheckHook
+  ];
+
+  env.__EGL_VENDOR_LIBRARY_DIRS = "${pkgs.mesa}/share/glvnd/egl_vendor.d";
+
+  meta = {
+    description = "Plugin to test PyVista plot outputs";
+    homepage = "https://github.com/pyvista/pytest-pyvista";
+    changelog = "https://github.com/pyvista/pytest-pyvista/releases/tag/${src.tag}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ qbisi ];
+  };
+}
