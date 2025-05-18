1fb1550376db8a3761cbf7ed365300706e1696bb
qbisi <qbisicwate@gmail.com>
2025-05-18 12:52:41 +0800

    trame-vtk: fix

--- pkgs/python-by-name/trame-vtk/package.nix
+++ pkgs/python-by-name/trame-vtk/package.nix
@@ -1,7 +1,8 @@
 {
   lib,
+  python,
   buildPythonPackage,
-  fetchFromGitHub,
+  fetchPypi,
   setuptools,
   trame-client,
 }:
@@ -9,19 +10,16 @@
 buildPythonPackage rec {
   pname = "trame-vtk";
   version = "2.8.15";
-  pyproject = true;
+  format = "wheel";
 
-  src = fetchFromGitHub {
-    owner = "kitware";
-    repo = "trame-vtk";
-    tag = "v${version}";
-    hash = "sha256-2t8Lc/JQ8oQCkIPRN8uOKhEabNdDAnM/tr+3cC+Au14=";
+  src = fetchPypi {
+    pname = "trame_vtk";
+    inherit version format;
+
+    python = "py3";
+    dist = "py3";
+    hash = "sha256-q/fSPSZ90VuH20skkwKgrqhXLCYNIF4mTbpYNrOtySs=";
   };
-
-  postPatch = ''
-    sed -i '/^\[tool\.setuptools\.packages\.find\]/a include = ["trame*"]' pyproject.toml
-    find trame -type f -name '__init__.py' -delete
-  '';
 
   build-system = [ setuptools ];
 
@@ -30,6 +28,11 @@ buildPythonPackage rec {
   ];
 
   pythonImportsCheck = [ "trame_vtk" ];
+
+  postFixup = ''
+    rm -rf $out/${python.sitePackages}/{tests,examples}
+    find $out/${python.sitePackages}/trame -type f -name '__init__.*' -delete
+  '';
 
   meta = {
     description = "VTK widgets for trame";
