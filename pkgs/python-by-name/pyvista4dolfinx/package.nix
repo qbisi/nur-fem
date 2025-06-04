c892a20387bba8311353b5c2cdb1ab74572e3613
qbisi <qbisicwate@gmail.com>
2025-06-04 12:04:51 +0800

    pyvista4dolfinx: init at 0.9.1

---
+++ pkgs/python-by-name/pyvista4dolfinx/package.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitLab,
+  setuptools,
+  fenics-dolfinx,
+  pyvista,
+  pyvistaqt,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "pyvista4dolfinx";
+  version = "0.9.1";
+  pyproject = true;
+
+  src = fetchFromGitLab {
+    owner = "fenicsx4flow";
+    repo = "pyvista4dolfinx";
+    tag = version;
+    hash = "sha256-msr9CNiGfIlrr1xeDhHBFTapa3ZlmtirKCW/ECNEaz0=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    pyvista
+    pyvistaqt
+    fenics-dolfinx
+  ];
+
+  pythonImportsCheck = [ "pyvista4dolfinx" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Re-introduction of a simple plot function for Dolfinx";
+    homepage = "https://gitlab.com/fenicsx4flow/pyvista4dolfinx";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ qbisi ];
+  };
+}
