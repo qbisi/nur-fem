a3bb726c1e8b074391abb71211c98c30fa943f14
qbisi <qbisicwate@gmail.com>
2025-06-07 18:24:09 +0800

    kahip: 3.18 -> 3.19

--- pkgs/by-name/kahip/package.nix
+++ pkgs/by-name/kahip/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch2,
   cmake,
   mpi,
   metis,
@@ -12,27 +11,21 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "kahip";
-  version = "3.18";
+  version = "3.19";
 
   src = fetchFromGitHub {
     owner = "KaHIP";
     repo = "KaHIP";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-l8DhVb2G6pQQcH3Wq4NsKw30cSK3sG+gCYRdpibw4ZI=";
+    hash = "sha256-PySbhryPSqr2fzInYPaNQzy4MBuinJ4XJ38zxAa0lCE=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      url = "https://github.com/KaHIP/KaHIP/commit/9d4978c7540a1ccbc9807367d6e3852114e86567.patch?full_index=1";
-      hash = "sha256-nIJL0YmVp9+JUhzEXjoabD1qNEnhtrBnjMWnitYt0eU=";
-    })
-  ];
 
   nativeBuildInputs =
     [ cmake ]
     ++ lib.optionals pythonSupport [
       python3Packages.python
       python3Packages.pybind11
+      python3Packages.pythonImportsCheckHook
     ];
 
   buildInputs = [
@@ -44,12 +37,6 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "64BITMODE" isILP64)
     (lib.cmakeBool "BUILDPYTHONMODULE" pythonSupport)
     (lib.cmakeFeature "CMAKE_INSTALL_PYTHONDIR" python3Packages.python.sitePackages)
-  ];
-
-  doInstallCheck = pythonSupport;
-
-  nativeInstallCheckInputs = lib.optionals pythonSupport [
-    python3Packages.pythonImportsCheckHook
   ];
 
   pythonImportsCheck = [ "kahip" ];
