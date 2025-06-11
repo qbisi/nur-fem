1ab3fc823c9d8808fd96fc58bc575265f98b493a
qbisi <qbisicwate@gmail.com>
2025-06-12 03:25:41 +0800

    paraview: init at 6.0.0-RC1

--- pkgs/by-name/paraview/package.nix
+++ pkgs/by-name/paraview/package.nix
@@ -5,6 +5,8 @@
   cmake,
   ninja,
   vtk,
+  catalyst,
+  protobuf,
   mpi,
   python3Packages,
   withQt5 ? false,
@@ -29,24 +31,39 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "paraview";
-  version = "5.13.3";
+  version = "6.0.0-RC1";
 
   src = fetchurl {
     url = "https://www.paraview.org/files/v${lib.versions.majorMinor finalAttrs.version}/ParaView-v${finalAttrs.version}.tar.xz";
-    hash = "sha256-O9MbtW4Hqiryo3mJV0W7xDDFZVGKNj2TXy78NbB23wk=";
+    hash = "sha256-5EI02JrrfTqLH8J1xZO7D/Q+hY4pVcFOhvJE4ilR0vk=";
   };
 
   cmakeFlags = [
     (lib.cmakeBool "PARAVIEW_VERSIONED_INSTALL" false)
     (lib.cmakeBool "PARAVIEW_USE_EXTERNAL_VTK" true)
-    (lib.cmakeBool "PARAVIEW_BUILD_WITH_EXTERNAL" true)
+    # (lib.cmakeBool "PARAVIEW_ENABLE_EXAMPLES" true)
+    (lib.cmakeBool "PARAVIEW_ENABLE_CATALYST" true)
     (lib.cmakeBool "PARAVIEW_ENABLE_VISITBRIDGE" true)
+    (lib.cmakeBool "PARAVIEW_ENABLE_ADIOS2" true)
+    (lib.cmakeBool "PARAVIEW_ENABLE_FFMPEG" true)
+    (lib.cmakeBool "PARAVIEW_ENABLE_FIDES" true)
+    # (lib.cmakeBool "PARAVIEW_ENABLE_GDAL" true)
+    (lib.cmakeBool "PARAVIEW_ENABLE_OPENTURNS" true)
+    # (lib.cmakeBool "PARAVIEW_ENABLE_PDAL" true)
+    (lib.cmakeBool "PARAVIEW_ENABLE_XDMF3" true)
+    # (lib.cmakeBool "PARAVIEW_PLUGINS_DEFAULT" true)
+    # (lib.cmakeBool "PARAVIEW_PLUGIN_dsp_enable_audio_player" true)
+    # (lib.cmakeBool "PARAVIEW_PLUGIN_ENABLE_NetCDFTimeAnnotationPlugin" true)
+    # (lib.cmakeBool "PARAVIEW_PLUGIN_ENABLE_pvNVIDIAIndeX" true)
+    # (lib.cmakeBool "PARAVIEW_PLUGIN_ENABLE_CDIReader" true) # https://gitlab.dkrz.de/mpim-sw/libcdi
+    # (lib.cmakeBool "PARAVIEW_PLUGIN_ENABLE_PythonQtPlugin" true)
     (lib.cmakeBool "PARAVIEW_USE_PYTHON" pythonSupport)
-    (lib.cmakeBool "PARAVIEW_ENABLE_WEB" pythonSupport)
     (lib.cmakeBool "PARAVIEW_USE_MPI" mpiSupport)
+    (lib.cmakeBool "PARAVIEW_USE_QT" (withQt5 || withQt6))
     (lib.cmakeFeature "CMAKE_INSTALL_BINDIR" "bin")
     (lib.cmakeFeature "CMAKE_INSTALL_LIBDIR" "lib")
     (lib.cmakeFeature "CMAKE_INSTALL_INCLUDEDIR" "include")
+    (lib.cmakeFeature "CMAKE_INSTALL_DOCDIR" "share/doc/ParaView")
   ];
 
   nativeBuildInputs = [
@@ -57,9 +74,11 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     vtkPackages.vtk
+    vtkPackages.qtPackages.qt5compat
+    catalyst
+    protobuf
+    # python3Packages.cftime
   ];
-
-  dontWrapQtApps = true;
 
   meta = {
     homepage = "https://www.paraview.org";
